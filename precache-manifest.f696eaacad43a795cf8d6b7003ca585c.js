c051e8d8679c92e797beb4dfdbf2a043d7b9eb08
Pavel <pates1993@mail.ru>
2021-05-09 14:36:55 +0300

    Updates

---
+++ precache-manifest.f696eaacad43a795cf8d6b7003ca585c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8b11b9301b0b9ac9117d9288bf5f7f",
+    "url": "/ignatHomeWork/index.html"
+  },
+  {
+    "revision": "4410fcc11ddbf23c3a9e",
+    "url": "/ignatHomeWork/static/css/main.40a4a98f.chunk.css"
+  },
+  {
+    "revision": "4612c67f9f33688cc758",
+    "url": "/ignatHomeWork/static/js/2.06509705.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ignatHomeWork/static/js/2.06509705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4410fcc11ddbf23c3a9e",
+    "url": "/ignatHomeWork/static/js/main.635efbd1.chunk.js"
+  },
+  {
+    "revision": "9a7e1f7246caf151a7f1",
+    "url": "/ignatHomeWork/static/js/runtime-main.0768d291.js"
+  }
+]);
